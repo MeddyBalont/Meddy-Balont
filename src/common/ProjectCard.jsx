d143f41274054439cc8cdf0b0ce0ffddf575c4ae
MeddyBalont <Meddy.Balont@hotmail.com>
2024-10-14 20:51:03 +0200

    Card terminé

--- src/common/ProjectCard.jsx
+++ src/common/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ProjectCard({ src, link, h3, p }) {
      return (
-          <a href={link}>
+          <a href={link} target="_blank">
                <img className="hover" src={src} alt={`${h3} logo`} />
                <h3>{h3}</h3>
                <p>{p}</p>
